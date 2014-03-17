a340c0cc313f98b543e5affbda5fdc7d44b6f242
Chris Kincanon <Catsith@gmail.com>
2014-03-16 21:29:10 -0700

    adding room support

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,15 +2,23 @@
 
 angular.module('firechatApp')
   .controller('MainCtrl', function ($scope) {
-    function makeid(){
+    function makeid(size){
       var text = '';
       var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-      for( var i=0; i < 5; i++ ){
+      for( var i=0; i < size; i++ ){
         text += possible.charAt(Math.floor(Math.random() * possible.length));
       }
       return text;
-    }
+    };
 
-    $scope.newRoom = makeid();
-    $scope.rooms = ['123','abc'];
+    $scope.addChat = function(){
+      if(jQuery.inArray($scope.newRoom, $scope.rooms) == -1){
+        $scope.rooms.push($scope.newRoom);
+      };
+      window.location = "#/chat/" + $scope.newRoom;
+      $scope.newRoom = makeid(10);
+    };
+
+    $scope.newRoom = makeid(10);
+    $scope.rooms = [];
   });
