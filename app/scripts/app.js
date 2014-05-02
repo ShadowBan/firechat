a015539b5c32ea7d72e0e2418245b3189766fea3
Chris Kincanon <Catsith@gmail.com>
2014-05-02 16:31:57 -0700

    new build

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,13 +13,11 @@ angular.module('firechatApp', [
     $urlRouterProvider.otherwise('/');
     
     $stateProvider
-        // HOME STATES AND NESTED VIEWS ========================================
         .state('home', {
             url: '/',
             templateUrl: 'views/main.html',
             controller: 'MainCtrl'
         })
-        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
         .state('chat', {
           url: '/chat/:roomId', 
           templateUrl: 'views/chat.html',
